019049993a2a3a2aa976fd91fdc28351cc1d3079
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-02-10 14:12:42 -0300

    feat

--- src/PublicRoute.tsx
+++ src/PublicRoute.tsx
@@ -4,13 +4,20 @@ import { Link } from "react-router-dom";
 export const PublicRoute = ({ children }: { children: ReactElement }) => {
   return (
     <>
-      <header className={"bg-blue-600 p-4 shadow-md mb-4"}>
+      <header className={"bg-blue-600 flex gap-3 p-4 shadow-md mb-4"}>
         <button
           className={
             "text-white bg-blue-800 hover:bg-blue-700 px-4 py-2 rounded-md"
           }
         >
           <Link to={"/admin"}>Acessar Admin</Link>
+        </button>
+        <button
+          className={
+            "text-white bg-blue-800 hover:bg-blue-700 px-4 py-2 rounded-md"
+          }
+        >
+          <Link to={"/"}>Acessar Ranking</Link>
         </button>
       </header>
       {children}
